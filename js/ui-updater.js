388bfc4b22dc685348ac0c5c82150f06b413f0c1
Mirzo Ulug'bek Xudoyberdiyev <ulugbekkhudoyberdiyev9008@gmail.com>
2023-10-27 09:56:02 +0500

    Check ui-updater function

--- js/ui-updater.js
+++ js/ui-updater.js
@@ -1,5 +1,7 @@
-const uiUpdater = (elements) => {
-  elements.forEach((element) => {});
+const uiUpdater = ({ result: elements, theme, gridSize }) => {
+  elements.forEach((element) => {
+    // console.log(element);
+  });
 };
 
 export default uiUpdater;
