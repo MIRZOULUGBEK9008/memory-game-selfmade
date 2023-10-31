f96592986994d013db510776df05198ea01f49c8
Mirzo Ulug'bek Xudoyberdiyev <ulugbekkhudoyberdiyev9008@gmail.com>
2023-10-31 14:30:20 +0500

    Refactor ui-updater structure

--- js/ui-updater.js
+++ js/ui-updater.js
@@ -3,43 +3,49 @@ import {
   elGameGrid,
   elGameGridIconTemplate,
   elGameGridNumberTemplate,
-  elGameStatus,
 } from "./html-elements.js";
 
 const { gridSize_4, gridSize_6 } = cssClassModifiers;
 
 const uiCleaner = () => {
   elGameGrid.innerHTML = null;
-  elGameStatus.innerHTML = null;
   elGameGrid.classList.remove(gridSize_4, gridSize_6);
 };
 
-const uiUpdater = ({ result: elements, theme, gridSize, numberOfPlayers }) => {
+const uiUpdater = ({ result: elements, theme, gridSize }) => {
   const fragmentElements = document.createDocumentFragment();
+
+  // Clean UI
   uiCleaner();
+
+  elements.forEach((element) => {
+    // Templates
+    const { number, icon } = {
+      number: elGameGridNumberTemplate.content.cloneNode(true),
+      icon: elGameGridIconTemplate.content.cloneNode(true),
+    };
+
+    const elementClone = theme === "icons" ? icon : number;
+    const elGridItem = elementClone.getElementById("gridItem");
+    const elGridButton = elGridItem.querySelector(".game-grid__button");
+
+    if (theme === "icons") {
+      const icon = elementClone.getElementById("icon");
+      icon.classList.add(`fa-${element}`);
+    } else {
+      const number = elementClone.getElementById("number");
+      number.innerText = element;
+    }
+
+    elGridButton.dataset.element = element;
+    elGridButton.onclick = () => console.log(1);
+    fragmentElements.appendChild(elementClone);
+  });
+
+  // Set grid size
   if (gridSize === "6") {
     elGameGrid.classList.add(gridSize_6);
   } else elGameGrid.classList.add(gridSize_4);
-
-  elements.forEach((element) => {
-    if (theme === "icons") {
-      const elementClone = elGameGridIconTemplate.content.cloneNode(true);
-      const elGridItem = elementClone.getElementById("gridItem");
-      const elGridButton = elGridItem.querySelector(".game-grid__button");
-      const icon = elementClone.getElementById("icon");
-      icon.classList.add(`fa-${element}`);
-      elGridButton.dataset.element = element;
-      fragmentElements.appendChild(elementClone);
-    } else {
-      const elementClone = elGameGridNumberTemplate.content.cloneNode(true);
-      const elGridItem = elementClone.getElementById("gridItem");
-      const elGridButton = elGridItem.querySelector(".game-grid__button");
-      const number = elementClone.getElementById("number");
-      number.innerText = element;
-      elGridButton.dataset.element = element;
-      fragmentElements.appendChild(elementClone);
-    }
-  });
 
   // Add element to DOM
   elGameGrid.append(fragmentElements);
